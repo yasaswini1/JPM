88ab4f3c2fbfa97944b02c316a6d28c350193c36
Vineel Arepalli <vineel@Vineels-MacBook-Pro.local>
2024-06-15 13:13:24 +0530

    SingUp

--- frontend/my-react-app/src/App.test.js
+++ frontend/my-react-app/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import SignUp from './components/SignUp';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<SignUp />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
